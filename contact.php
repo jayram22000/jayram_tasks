ec142335302cdd2037720c428de42a2a984492d2
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-02-01 10:29:20 +0200

    Initial commit

---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php 
+$title= "Contact Us: Contact Hamk";
+include "header.php" ?>
+<h2> Contact us </h2>
+<p> ootstrap is the sixth-most-starred project on GitHub, with more than 135,000 stars, behind freeCodeCamp (almost 307,000 stars) and marginally behind Vue.js framework.[2] According to Alexa Rank, Bootstrap getbootstrap.com
+         is in the top-2000 in US while vuejs.org is in top-7000 in US.</p>
+
+<?php include "footer.php"?>
