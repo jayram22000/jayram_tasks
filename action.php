ee37d4ea1aabeb9152ffa9eca771d6d2f436c568
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-02-07 10:10:42 +0200

    create a action file

---
+++ action.php
@@ -0,0 +1,19 @@
+<?php
+$title= "Action"; 
+include "header.php";
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$dob= $_POST['bdate'];
+$color= $_POST['color'];
+
+echo "<h3> Your name is $fname $lname /.
+Your favourite color is $color and your date of birth is $dob</h3>";
+
+
+
+
+
+
+?>
+
+<?php include "footer.php" ?>   <!-- universal footer !-->
--- variable.php
+++ variable.php
@@ -1,11 +1,17 @@
 <?php
-$title= "Execise: NO:3";
+$title= "Execise:NO:3";
  include "header.php"?><!-- universal header !-->
+<h3>  Exercise3.In-class Task Variable & Operators 07.02.2023 (variable.php)</h3>
 
 
+ <h2>
+ Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print usingtag: Hello …., You are welcome to my site. <h2> 
 
-
-
-
-
+<form action="action.php" method="post">
+    Firstname: <input type="text" name="fname"> <br>
+    Lastname: <input type="text"  name="lname"> <br>
+    Birth Date: <input type="date"  name="bdate"> <br>
+    select fav color: <input type="color" name="color"> <br>
+    <input type="submit" value="Submit">
+</form>
 <?php include "footer.php" ?>   <!-- universal footer !-->
