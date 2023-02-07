2fad5b7a24d57f28fd5a5606a965e723fb3313de
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-02-07 10:36:53 +0200

    add the bootstrap

--- variable.php
+++ variable.php
@@ -5,13 +5,24 @@ $title= "Execise:NO:3";
 
 
  <h2>
- Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print usingtag: Hello …., You are welcome to my site. <h2> 
+ 1.Create a simple html form to get Firstname and Lastname from the user and use echo echo statement to print usingtag: Hello …., You are welcome to my site. <h2> 
 
 <form action="action.php" method="post">
-    Firstname: <input type="text" name="fname"> <br>
-    Lastname: <input type="text"  name="lname"> <br>
-    Birth Date: <input type="date"  name="bdate"> <br>
-    select fav color: <input type="color" name="color"> <br>
+    <div class="row">
+    <div class="col">
+    <input type="text" name="fname" required placeholder="First name" class="form-control"> 
+</div>
+<div class="col">
+    <input type="text"  name="lname"required placeholder="Last name" class="form-control">
+</div>
+<div class="col">
+    <input type="text"  name="bdate"required placeholder="birthdate" class="form-control">
+</div>
+<div class="col">
+    <input type="text"  name="color"required placeholder="select fav color" class="form-control">
+</div>
+    
     <input type="submit" value="Submit">
+</div>
 </form>
 <?php include "footer.php" ?>   <!-- universal footer !-->
