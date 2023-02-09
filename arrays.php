72c1e93143009c298e29a065b9cce20e066bb627
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-02-09 11:05:19 +0200

    corection execise 5(arrays.php)

--- arrays.php
+++ arrays.php
@@ -4,9 +4,12 @@ include "header.php" ?>
 <h2>1.Write a php script to display courses as list. Use 
 $courses=array("PHP", "HTML", "JavaScript", "CMS", "Project")</h2>
 <?php
-$courses=("PHP","HTML","JavaScript","CMS","Project");
-for ($i=0; < count($courses);$i++){
-    echo $courses[$i];
+$courses3 = array("PHP", "HTML", "JavaScript", "CMS", "Project");
+sort($courses3);
+$clength = count($courses3);
+for($x = 0; $x < $clength; $x++){
+    echo $courses3[$x];
+    echo "<br>";
 }
 ?>
 <h2>2.The unset() function is used to remove element from the array.
@@ -14,5 +17,5 @@ The var_dump() function is used to dump information about a variable.
 array_values() is an inbuilt function that 
 returns all the values of an array and not the keys. </h2>
 <?php
-
+?>
 <?php include "footer.php" ?>
