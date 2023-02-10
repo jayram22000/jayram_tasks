03df5f51f72e256d9932893a510bf6e03ace4715
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-02-10 12:41:38 +0200

    correction on ifelse statement for voter

--- action1.php
+++ action1.php
@@ -4,8 +4,12 @@ include "header.php";
 $fname = $_POST['fname'];
 $lname = $_POST['lname'];
 $age= $_POST['age'];
-echo "<h4> Your voter name is $fname $lname /.
- and voter age $age</h4>";
+if($age>=18)
+echo "<h4> eligible for voting </h4>";
+elseif($age<18)
+echo "<h4> is not eligible for voting </4>";   
+
+
 ?>
 
 <?php include "footer.php" ?>   <!-- universal footer !-->
