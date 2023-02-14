108e425650d6d06c978ee6da0b99a02e14e1de55
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-02-14 20:29:14 +0200

    correction

--- crud1/upadtesingle.php
+++ crud1/upadtesingle.php
@@ -6,7 +6,7 @@ $a = $_GET['id'];
 $result = mysqli_query($conn,"Select * from studentinfo where id='$a' ");
 $row = mysqli_fetch_array($result);
 ?>
-<h2> Upade your information below </h2>
+<h2> Update your information below </h2>
 <form name="update" method="post" action="">
     <input type="text" name="fname" placeholder="First name" required value="<?php echo $row['fname'];?>"><br><br>
     <input type="text" name="lname" placeholder="Last name" required value="<?php echo $row['lname'];?>"><br><br>
@@ -16,7 +16,7 @@ $row = mysqli_fetch_array($result);
 <option value="BBCAP21"> BBCAP21</option>
 <option value="others"> others</option>
 </select><br><br>
-<input type="submit" value="Upadte your info" name="update"> <br><br>
+<input type="submit" value="Update your info" name="update"> <br><br>
 <input type="submit" value="Delete your info" name="delete"> <br><br>
 
 </form>
