e54c6438181039400ff0c1b1008c1a7967f952c9
jayram22000 <117517016+jayram22000@users.noreply.github.com>
2023-02-20 12:04:55 +0200

    crete js file done some function of js

--- ex1.php
+++ ex1.php
@@ -12,12 +12,36 @@ echo "Hello world! My name is \"David\"";
 ?>
 <h1>3.3 Write the PHP code in to display the current date. </h1>
 <?php echo date("d.m.Y")?>
+<script>
+    document.write("Hello world this is JavaScript");
+    </script>
+    <noscript>
+        please enable JavaScript to view this content,
+</noscript>
+<input type="button" onlick="hello()" value="Click Me">
+        
 <h3> 3.4 $title = "PHP is interesting”. Put this variable as a title marked as h1 (heading 1) in your HTML document</h3> 
 <?php 
 $title1 = "PHP is interesting";
  echo "<h1>" . $title1 . "</h1>";
  ?>
+ <script>
+    //window.alert
+    //window.alert("The will trigger an alert box");
+   // window.alert(5+10);
+   // document.write
+   document.write("this a interesting")
+    </script>
+
 <h3> 3.5 $g1=5, $g2=4, $g3=5. These are the grades for 3 students in the course. Use HTML table into echo and include 3 columns S.n., Name, and grade. Your table should look like the one below.</h3>
+<button onclick="add()">Click to Add</button>
+<hr>
+<p id="place1"style="color:red; background-color:yellow;"></p>
+
+<span id="place2"style="color:red; background-color:green;"></p> </span>
+<script>
+    document.getElementById("place1")
+    .innerHTML= "this will go to place";
 <?php
 $g1 = 5;
 $g2 = 4;
@@ -39,4 +63,8 @@ echo "
 
 <h3> 4.Take a screenshot that verifies </h3>
 <img src="image.png" alt="">
+<h2> changing background-color </h2>
+<form>
+<input type="color" name="background" onchange="changeColor('background',this.value)">
+</form>
 <?php include "footer.php" ?>
---
+++ js/site.js
@@ -0,0 +1,15 @@
+// a simple function that triggers alert box  
+function hello(){
+    alert("welcome to my websites");
+}
+// a simple function to add 2 numbers
+function add(){
+    document.write(5+6);
+}
+//function to change the background color of the page 
+function changeColor(where,newColor){
+    if (where=="background"){
+        document.body.style.backgroundColor=newColor;
+    }
+
+}
